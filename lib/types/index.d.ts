2702457e3bccd16db8ed7760856680b36a4a7d22
dmh <5150636+dmh@users.noreply.github.com>
2024-01-12 14:33:16 +0200

    [TASK] big refactoring, update dependencies

---
+++ lib/types/index.d.ts
@@ -0,0 +1,13 @@
+declare module '@hubspot/local-dev-lib/personalAccessKey';
+declare module '@hubspot/local-dev-lib/config';
+declare module '@hubspot/cli-lib/fileMapper.js';
+declare module '@hubspot/cli-lib/api/fileMapper.js';
+declare module '@hubspot/local-dev-lib/fileMapper';
+declare module '@hubspot/local-dev-lib/fs';
+declare module '@hubspot/local-dev-lib/ignoreRules';
+declare module '@hubspot/local-dev-lib/path';
+declare module '@hubspot/cli-lib/logger.js';
+declare module '@hubspot/cli-lib/lib/uploadFolder.js';
+declare module '@hubspot/cli-lib/api/lighthouseScore.js';
+declare module '@hubspot/cli-lib/api/marketplaceValidation.js';
+declare module '@hubspot/cli-lib/lib/watch.js';
